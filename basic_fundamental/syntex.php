223bdcf0dbc75a369057394e102c5ecef35690e0
GauravGoswami12967 <gaurav12967@gmail.com>
2024-05-27 12:03:02 +0530

    fisrt commit

---
+++ basic_fundamental/syntex.php
@@ -0,0 +1,48 @@
+<!--
+
+PHP personal Home Page
+
+PHP is an acronym for "PHP: Hypertext Preprocessor"
+PHP invent in 1994 by Rasmus Lerdorf
+PHP is a widely-used, open source server side scripting language
+PHP can generate dynamic page content
+PHP is a powerful tool for making dynamic and interactive Web pages.
+PHP scripts are executed on the server
+PHP is free to download and use
+PHP files can contain text, HTML, CSS, JavaScript, and PHP code
+PHP files have extension ".php"
+
+
+wHY ?
+
+INHERITANCE FROM C 
+FREE OPEN SOURCE
+LARGE DOCUMANTATION
+SUPPORT ALL OS
+SUPPORT ALL DATABASE
+MOST OF ALL SERVER SUPPORT
+FRAMEWORK & CMS
+
+-->
+
+<?php
+
+// SINGLE COMMENT
+
+/*
+	MULTILINE COMMENT
+*/
+
+
+$name="Raj nagar";
+
+echo $name;
+
+
+echo "<h1> Hello hi i am php </h1>";
+
+echo "<h1> hello " . $name . "</h1>";
+
+?>
+
+
