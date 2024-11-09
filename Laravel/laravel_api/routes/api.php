910cc44d7ebe893021612a4767eff517084f4b45
GauravGoswami12967 <gaurav12967@gmail.com>
2024-11-09 13:33:21 +0530

    fisrt

---
+++ Laravel/laravel_api/routes/api.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\student_controller;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/', function () {
+    echo "Hello";
+});
+
+
+
+Route::get('/student',[student_controller::class,'allshow']); // datafetch  http://127.0.0.1:8000/api/student/
+Route::get('/student/{id}',[student_controller::class,'single_show']); // edit 
+
+Route::delete('/student/{id}',[student_controller::class,'destroy']); // delete
+
+Route::post('/insertstudent',[student_controller::class,'store']); // insert
+
+Route::put('/updatestudent/{id}',[student_controller::class,'update']); // update
+
+Route::get('/search',[student_controller::class,'search']); // search
+
+Route::put('/updatestatus/{id}',[student_controller::class,'updatestatus']); // block unblock
+Route::post('/login',[student_controller::class,'studentlogin']); // login
+
+/*
+
+
+
+
+
+*/
